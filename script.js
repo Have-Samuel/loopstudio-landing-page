e77eedc774a0c44a4daed48c36fcd879d12c0781
Have Samuel <samiehave9@gmail.com>
2023-11-17 16:16:05 +0300

    Changing the variables

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Mobile MenuPopup
-const parentElement = document.querySelector('.header');
 const menuIcon = document.querySelector('.hamburger-menu');
 // capturing the third child of the document object
-parentElement.insertBefore(menuIcon, parentElement.children[3]);
 
 const navbar = document.querySelector('.navbar');
 
