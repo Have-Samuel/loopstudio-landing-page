3a42296b6ba0d84fc8f42036bb5150ee2f2e42d7
Have Samuel <samiehave9@gmail.com>
2023-11-19 18:47:50 +0300

    Deleting the unwanted code

--- script.js
+++ script.js
@@ -2,13 +2,7 @@
 const menuIcon = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar');
 
-const listItems = document.querySelectorAll('.nav-list > li');
-listItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    navbar.classList.remove('change');
-  });
-});
-
 menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('change');
   navbar.classList.toggle('change');
 });
