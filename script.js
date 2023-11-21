000b0d1c33645ee026778255770e3bbc5d11b26d
Have Samuel <samiehave9@gmail.com>
2023-11-21 13:04:34 +0300

    Adding the a new image

--- script.js
+++ script.js
@@ -11,34 +11,42 @@ menuIcon.addEventListener('click', () => {
 const projects = [
   {
     title: 'deep earth',
+    mobileImage: './images/mobile/image-deep-earth.jpg',
     featuredImage: './images/desktop/image-deep-earth.jpg',
   },
   {
     title: 'night arcade',
+    mobileImage: './images/mobile/image-night-arcade.jpg',
     featuredImage: './images/desktop/image-night-arcade.jpg',
   },
   {
     title: 'soccer team vr',
+    mobileImage: './images/mobile/image-soccer-team.jpg',
     featuredImage: './images/desktop/image-soccer-team.jpg',
   },
   {
     title: 'the grid',
+    mobileImage: './images/mobile/image-grid.jpg',
     featuredImage: './images/desktop/image-grid.jpg',
   },
   {
     title: 'from up above vr',
+    mobileImage: './images/mobile/image-from-above.jpg',
     featuredImage: './images/desktop/image-from-above.jpg',
   },
   {
     title: 'pocket borealis',
+    mobileImage: './images/mobile/image-pocket-borealis.jpg',
     featuredImage: './images/desktop/image-pocket-borealis.jpg',
   },
   {
     title: 'the curiosity',
+    mobileImage: './images/mobile/image-curiosity.jpg',
     featuredImage: './images/desktop/image-curiosity.jpg',
   },
   {
     title: 'make it fisheye',
+    mobileImage: './images/mobile/image-fisheye.jpg',
     featuredImage: './images/desktop/image-fisheye.jpg',
   },
 ];
@@ -47,7 +55,7 @@ const projects = [
 function renderCards(work) {
   return `
     <div class="card">
-      <img class="card-img" src="${work.featuredImage}" alt="Project 1" aria-hidden="true" />
+      <img class="card-img" src="${work.mobileImage}" alt="Project 1" aria-hidden="true" />
       <div class="card-content">
         <h2 class="title color-caption">${work.title}</h2>
       </div>
